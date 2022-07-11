af1fd6eec694fc61ff4cdd8644b7c21a260dfc6a
Michael Halpin <mhalpin613@gmail.com>
2022-07-10 22:15:07 -0400

    added some comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let playerScore = 0;
 let cpuScore = 0;
-let winner;
 
+// getting DOM elements
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
@@ -10,6 +10,7 @@ const playerMove = document.getElementById("playerChoice");
 const computerChoice = document.getElementById("computerChoice");
 const result = document.getElementById("result");
 
+// determines if the player won, lost, or tied the round
 function checkRoundWinner (playerChoice, cpuPick) {
     if (playerChoice === cpuPick) {
         return "Tie!";
@@ -22,35 +23,42 @@ function checkRoundWinner (playerChoice, cpuPick) {
     }
 };
 
+// checks if a player has reached a winning score and prompts user for restart
 function checkWinner (playerScore, cpuScore) {
     if (cpuScore >= 3) {
-        alert("CPU is the winner! Refresh page to restart.");
+        alert("CPU is the winner! Refresh the page to restart.");
     } else if (playerScore >= 3) {
-        alert("You are the winner! Refresh page to restart.");
+        alert("You are the winner! Refresh the page to restart.");
     }
 };
 
+// starts a round of the game
 function game(playerChoice) {
 
+    // gets the player and cpu choice and checks for the result
     const choices = ["Rock", "Paper", "Scissors"];
     let playerPick = playerChoice;
     let cpuPick = choices[Math.floor(Math.random() * 3)];
     let roundResult = checkRoundWinner(playerPick, cpuPick);
 
+    // updates the score if there is no tie
     if (roundResult == "You win") {
         playerScore++;
     } else if (roundResult == "You lose") {
         cpuScore++;
     }
 
+    // updates html
     score.innerHTML = playerScore + " - " + cpuScore;
     playerMove.innerHTML = "Your choice: " + playerPick;
     computerChoice.innerHTML = "CPU choice: " + cpuPick;
     result.innerHTML = "Result : " + roundResult;
 
+    // check if the game should be ended
     checkWinner(playerScore, cpuScore);
 };
 
+// adds event listeners for the three options and starts the round with choice
 rock.addEventListener("click", () => {
     let playerChoice = "Rock";
     game(playerChoice);
